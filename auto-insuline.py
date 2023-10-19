71ff99d6053359ab2efd535129fd51413ca0fa32
bigmountarth <arturwielogorski@gmail.com>
2023-10-19 16:42:03 +0100

    nsuline project

---
+++ auto-insuline.py
@@ -0,0 +1,9 @@
+mystring = ('malwmrllpllallalwgpdpaaafvnqhlcgshlvealylvcgergffytpktrreaedlqvgqvelgggpgagslqplalegslqkrgiveqcctsicslyqlenycn')
+
+a = mystring
+
+print("Your string is : ",a)
+print("lsinsulin-seq-clean is : ",a[0:23])
+print("binsulin-seq-clean is : ",a[24:53])
+print("cinsulin-seq-clean is : ",a[54:88])
+print("ainsulin-seq-clean is : ",a[89:109])
